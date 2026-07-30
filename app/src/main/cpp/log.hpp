b0ecaa862eefb6ddfff6d9bbda43173e1f800ec5
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-07-30 02:17:50 +0100

    switch to string_view

--- app/src/main/cpp/log.hpp
+++ app/src/main/cpp/log.hpp
@@ -4,31 +4,32 @@
 
 #include <android/log.h>
 #include <format>
+#include <string_view>
 
 namespace log {
     template<typename... Args>
-    void log_internal(android_LogPriority prio, std::format_string<Args...> fmt, Args&&... args) {
+    void log_internal(android_LogPriority prio, std::string_view fmt, Args&&... args) {
         std::string str = std::format(fmt, std::forward<Args>(args)...);
         __android_log_write(prio, LOG_TAG, str.c_str());
     }
 
     template<typename... Args>
-    inline void debug(std::format_string<Args...> fmt, Args&&... args) {
+    inline void debug(std::string_view fmt, Args&&... args) {
         log_internal(ANDROID_LOG_DEBUG, fmt, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void info(std::format_string<Args...> fmt, Args&&... args) {
+    inline void info(std::string_view fmt, Args&&... args) {
         log_internal(ANDROID_LOG_INFO, fmt, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void warn(std::format_string<Args...> fmt, Args&&... args) {
+    inline void warn(std::string_view fmt, Args&&... args) {
         log_internal(ANDROID_LOG_WARN, fmt, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void error(std::format_string<Args...> fmt, Args&&... args) {
+    inline void error(std::string_view fmt, Args&&... args) {
         log_internal(ANDROID_LOG_ERROR, fmt, std::forward<Args>(args)...);
     }
 }
