7d748f431a400a3301a26036e7017497fe7b34e3
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-07-30 02:23:04 +0100

    2nd attempt fixing log stuff

--- app/src/main/cpp/log.hpp
+++ app/src/main/cpp/log.hpp
@@ -3,33 +3,32 @@
 #define LOG_TAG "GeodeLauncher-Fix"
 
 #include <android/log.h>
-#include <format>
-#include <string_view>
+#include <fmt/core.h>
 
 namespace log {
     template<typename... Args>
-    void log_internal(android_LogPriority prio, std::string_view fmt, Args&&... args) {
-        std::string str = std::format(fmt, std::forward<Args>(args)...);
+    void log_internal(android_LogPriority prio, fmt::string_view fmt, Args&&... args) {
+        std::string str = fmt::format(fmt, std::forward<Args>(args)...);
         __android_log_write(prio, LOG_TAG, str.c_str());
     }
 
     template<typename... Args>
-    inline void debug(std::string_view fmt, Args&&... args) {
-        log_internal(ANDROID_LOG_DEBUG, fmt, std::forward<Args>(args)...);
+    inline void debug(fmt::format_string<Args...> fmt, Args&&... args) {
+        log_internal(ANDROID_LOG_DEBUG, fmt.get(), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void info(std::string_view fmt, Args&&... args) {
-        log_internal(ANDROID_LOG_INFO, fmt, std::forward<Args>(args)...);
+    inline void info(fmt::format_string<Args...> fmt, Args&&... args) {
+        log_internal(ANDROID_LOG_INFO, fmt.get(), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void warn(std::string_view fmt, Args&&... args) {
-        log_internal(ANDROID_LOG_WARN, fmt, std::forward<Args>(args)...);
+    inline void warn(fmt::format_string<Args...> fmt, Args&&... args) {
+        log_internal(ANDROID_LOG_WARN, fmt.get(), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    inline void error(std::string_view fmt, Args&&... args) {
-        log_internal(ANDROID_LOG_ERROR, fmt, std::forward<Args>(args)...);
+    inline void error(fmt::format_string<Args...> fmt, Args&&... args) {
+        log_internal(ANDROID_LOG_ERROR, fmt.get(), std::forward<Args>(args)...);
     }
 }
